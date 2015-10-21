e579ce45387f909e9c53ca2977e3d602940b6ab0
Jayce Miller <jaycem@smu.edu>
2015-10-21 17:31:39 -0500

    Added routes and login/register functions

--- app/routers/api/v.10/User.php
+++ app/routers/api/v.10/User.php
@@ -1,25 +1,50 @@
 <?php
 
 // Import Dependencies
-require __DIR__ . '/vendor/autoload.php'; 
+require __DIR__ . '/vendor/autoload.php';
 
 // This is the user Controller, so define it as group User
 $app->group('/api/v1.0/User', function() use ($app) {
-	
-	$app->get('/home', function () use ($app){ 
-    	
+
+	$app->get('/home', function () use ($app){
+
     });//)->add($MIDDLEWARE_AUTH);
-	
-	$app->post('/login', function () use ($app){ 
-    	
+
+	$app->post('/login', function () use ($app){
+		$email = $app->request->post('email');
+		$password = $app->request->post('password');
+		$user = new \User($email);
+
+		if(\Token::tokenExists($email))
+			\Token::revokeTokenByEmail($email);
+
+		if($user->checkLogin($password)){
+			$_SESSION['auth_token'] = \Token::generateToken($email);
+			echo "$email logged in";
+		}
+		else{
+			echo "Invalid login";
+		}
+
+
     });
-	
-	$app->delete('/logout', function () use ($app){ 
-    	
+
+	$app->delete('/logout', function () use ($app){
+
     });
-	
-	$app->post('/register', function () use ($app){ 
-    	
+
+	$app->post('/register', function () use ($app){
+		$email = $app->request->post('email');
+		$password = $app->request->post('password');
+		$user = new \User($email);
+		if($user->create($email, $password, $firstname.$lastname, $phoneNumber)){
+			echo 'Account Created. Logging in... ';
+			self::signIn($email, $password);
+		}
+		else{
+			echo 'Account already exists';
+		}
+
     });
 
 });
