8f817e88c24a752a181aa72d6083e26e8e49508f
Chris Henk <cahenk95@gmail.com>
2015-11-04 00:02:18 -0600

    Implemented CRUD functions for Group, more verbose Database functions

--- app/lib/Database.php
+++ app/lib/Database.php
@@ -26,7 +26,7 @@ class Database{
 		return self::$connection;
 	}
 	
-	public static function prepareAssoc($query){
+	public static function prepare(&$query, Binding & $binding, $fetchMode=PDO::FETCH_NUM){
 		// Check that the calling class uses the CRUD interface
 		$trace = debug_backtrace();
 		if(!($trace[1]['class'] instanceof CRUD))
@@ -36,9 +36,67 @@ class Database{
 		if(!isset(self::$connection))
 			self::init();
 		
+		// Prepare query and set fetch mode to associative
 		$statement = self::$connection->prepare($query);
-		$statement->setFetchMode(PDO::FETCH_ASSOC);
+		$statement->setFetchMode($fetchMode);
+		
+		// If bindings were provided, apply them
+		if(isset($binding))
+			$binding->applyBindings($statement);
 		
 		return $statement;
+		
+	}
+	
+	public static function prepareAssoc(&$query, Binding & $binding){
+		self::prepare($query, Binding & $binding, PDO::FETCH_ASSOC);
+	}
+
+	public static function lastInsertId(){
+		if(isset(self::$connection))
+			return self::$connection->lastInsertId();
+		else
+			die('ERROR: No connection to get lastInsertId from...');
+	}
+
+}
+
+class Binding{
+	
+	private $bindings;
+	
+	public function __construct(array $bindings = array()){
+		$this->bindings = $bindings;	
+	}
+	
+	public function applyBindings(PDOStatement & $statement){
+		// Apply the bindings to the statement (if given)
+		if(isset($statement)){
+			foreach($bindings as $key => $val){
+				switch(count($val)){
+					case 1:
+					$statement->bindParam($val[0]);
+					break;
+					
+					case 2:
+					$statement->bindParam($val[0], $val[1]);
+					break;
+					
+					case 3:
+					$statement->bindParam($val[0], $val[1], $val[2]);
+					break;
+					
+					case 4:
+					$statement->bindParam($val[0], $val[1], $val[2], $val[3]);
+					break;
+					
+					default:
+					die('ERROR: No bindings were given....');
+				}
+			}
+		}
+		else{
+			die('ERROR: Valid PDOStatement not given...');
+		}
 	}
 }
--- app/models/Group.php
+++ app/models/Group.php
@@ -1,5 +1,4 @@
 <?php
-require_once('../lib/Model.php');
 
 class Group extends Model implements CRUD{
 	protected $creationTime;
@@ -8,13 +7,33 @@ class Group extends Model implements CRUD{
 	protected $creatorUserID;
   	protected $logo;
   	protected $groupID;
-  	protected $userIDs;
+	protected $userIDs;
+	protected $permissions;  
+	  
+	private static $binding;
 
 	public function __construct(array $args = array()){
 		parent::__construct($args, array(
 			array('groupID'),
 			array('name','creatorID')
 			));
+			
+		// Initialize MySQL bindings
+		if(!isset(self::$binding))
+			self::$binding = Binding(array(
+				':creatorID' => $this->creatorID,
+				':name' => $this->name,
+				':description' => $this->description,
+				':creationTime' => $this->creationTime,
+				':logo' => $this->logo
+			));
+			
+		// Initialize Permission Arrays
+		$this->permissions = array(
+			Permission::OWNER => array(),
+			Permission::ADMIN => array(),
+			Permission::MODERATOR => array()	
+		);
 	}
 
 	public function getCreationTime(){
@@ -73,19 +92,69 @@ class Group extends Model implements CRUD{
 		$this->userIDs = $userIDs;
 	}
 	
+	/**
+		TODO: These functions aren't performing any level of error checking. Add at later date
+	*/
+	
 	public function create(){
+		// Prepare sql statement
+		$stmt = Database::prepareAssoc("INSERT INTO GroupDetails VALUES(:creatorID, :name, :description, :creationTime, :logo);", self::$binding);
 		
+		// Run the initial query and store the autoincremented group id
+		$stmt->execute();
+		$this->groupID = Database::lastInsertId();
+		
+		$stmt = Database::prepareAssoc("INSERT INTO Group VALUES(:groupID, :creatorID);
+			INSERT INTO Permissions VALUES(:groupID, :creatorID, '".Permission::OWNER."');", self::$binding);
+  
+  		// Run the secondary queries
+  		$stmt->execute();
 	}
 	
 	public function update(){
-		
+		Database::prepareAssoc("UPDATE GroupDetails SET creatorID=:creatorID, name=:name,
+			description=:description, creationTime=:creationTime, logo=:logo WHERE groupID=:groupID", 
+			self::$bindings)->execute();
 	}
 	
 	public function delete(){
-		
+		// Remove any data linking back to the group id then remove the group itself (from GroupDetails)
+		Database::prepareAssoc("DELETE FROM Permissions WHERE groupID=:groupID;
+			DELETE FROM EventGroups WHERE groupID=:groupID;
+			DELETE FROM Group WHERE groupID=:groupID;
+			DELETE FROM GroupDetails WHERE groupID=:groupID;", self::$bindings)->execute();
 	}
 
 	public function load(){
+		// Load group details
+		$stmt = Database::prepareAssoc("SELECT * FROM GroupDetails WHERE groupID=:groupID;", self::$bindings);
+		$stmt->execute();
+		$this->loadInto($stmt->fetch());
+		
+		// Load group members
+		$stmt = Database::prepareAssoc("SELECT `userID` FROM Group WHERE groupID=:groupID", self::$bindings);
+		$stmt->execute();
+		$this->loadAllInto($stmt->fetchAll(), array('userIDs'));
+		
+		// Load group member permissions
+		$stmt = Database::prepareAssoc("SELECT `userID`, `permission` FROM Permissions WHERE groupID=:groupID", self::$bindings);
+		$stmt->execute();
+		$data = $stmt->fetchAll();
+		foreach($data as $row)
+			$this->permissions[$row['permission']][] = $row['userID'];
+		
+		// Load the group's events
+		$stmt = Database::prepareAssoc("SELECT `eventID` FROM EventGroups WHERE groupID=:groupID", self::$bindings);
+		$stmt->execute();
+		$this->loadAllInto($stmt->fetchAll(), array('eventIDs'));
 		
 	}
+}
+
+class Permission{
+	
+	const OWNER = 'OWNER';
+	const ADMIN = 'ADMIN';
+	const MODERATOR = 'MODERATOR';
+	
 }
