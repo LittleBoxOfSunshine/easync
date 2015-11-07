0fd2d6c46bf0bac98602f9739b3261530255f5d0
Jayce Miller <jaycem@smu.edu>
2015-11-07 17:12:13 -0600

    adding to User

--- app/models/User.php
+++ app/models/User.php
@@ -18,35 +18,43 @@ class User extends Model implements CRUD{
 				array('userID'),
 				array('email', 'password')
 			),
-			$fromUserInput	
+			$fromUserInput
 		);
 	}
 
 	public function login(){
-		
+		return count(\Database::query("SELECT `email` FROM User WHERE
+			`email`='$email' AND `password`='$password' LIMIT 1;")) > 0;
+
 	}
-	
-	public function register(){
-		
-	}
-	
+
 	public function logout(){
-		
+
 	}
-	
+
+	public function exists(){
+			$stmt = \Database::prepareAssoc("SELECT `email` FROM User WHERE `email`='$email'");
+			$stmt->execute();
+	}
+
 	public function create(){
-		
+		// Prepare sql statement
+		$stmt = Database::prepareAssoc("INSERT INTO User VALUES(:email, :password, :firstname, :lastname);", self::$binding);
+
+		// Run the initial query and store the autoincremented group id
+		$stmt->execute();
+
 	}
-	
+
 	public function update(){
-		
+
 	}
-	
+
 	public function delete(){
-		
+
 	}
 
 	public function load(){
-		
+
 	}
 }
