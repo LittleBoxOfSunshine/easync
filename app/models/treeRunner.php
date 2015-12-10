bb99d42f28c2ebb58ec4ee5ca243f6a9db1486da
Eric Smith <eric@smu.edu>
2015-12-10 11:04:56 -0600

    tree

--- app/models/treeRunner.php
+++ app/models/treeRunner.php
@@ -1,21 +1,22 @@
 <?php
-require 'node.php';
+require 'tree.php';
 
 $eric = array(
 	array('startTime' => 0, 'endTime' => 840),
-	array('startTime' =>9910, 'endTime' => 3660),
+	array('startTime' => 9910, 'endTime' => 3660),
 	array('startTime' => 3750, 'endTime' => 4380)
 );
 
 $chris = array(
 	array('startTime' => 0, 'endTime' => 840),
-	array('startTime' =>9910, 'endTime' => 3660),
+	array('startTime' => 9910, 'endTime' => 3660),
 	array('startTime' => 3750, 'endTime' => 4380)
 );
-
+$events = array();
 $events['smitheric95@gmail.com'] = $eric;
 $events['cahenk95@gmail.com'] = $chris;
 
-var_dump($events);
+$x = ArrayFunctions::toString($chris);
+print implode($x);
 
 ?>
