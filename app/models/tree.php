aee037d65af713f715c904725e6c775fd73d55e3
Chris Henk <cahenk95@gmail.com>
2015-12-11 17:18:34 -0600

    removed old code

--- app/models/tree.php
+++ app/models/tree.php
@@ -22,8 +22,6 @@ class IntervalObject{
         if(count($this->children) > 0 ){
             // sort by depth
             usort($this->children, 'self::compareDepth');
-            //var_dump($this->children);
-            //die();
 
             $parents = array_merge($parents, $this->people);
 
@@ -49,39 +47,6 @@ class IntervalObject{
                 $ret[] = array('people' => $peeps, 'startTime' => $this->getLower(), 'endTime' => $this->getUpper());
         }
     }
-
-    /*
-	public function getTop(&$x, array & $ret=array()){
-		if(count($this->children) > 0){
-			// sort by depth
-			usort($this->children, 'self::compareDepth');
-			
-			foreach($this->children as $child){
-                $copy = [];
-
-                if(strcmp($this->people[0], 'ROOT') != 0)
-                    $copy = $this->people;
-
-				$child->getTop($x, $copy);
-
-                if(strcmp($this->people[0], 'ROOT') == 0) {
-                    $ret[] = $copy;
-                    echo "\n--------------ROOT LINE----------------\n";
-                }
-                else {
-                    if (!is_array($copy)) $copy = array($copy);
-                    $ret[] = array('people' => array_merge_recursive($this->people, $copy), 'startTime' => $this->getLower(), 'endTime' => $this->getUpper());
-                }
-
-				if(count($ret) >= $x)
-					break;	
-			}
-		}
-		else{
-			if(!is_array($this->people)) $this->people = array($this->people);
-			$ret[] = array('people' => $this->people, 'startTime' => $this->getLower(), 'endTime' => $this->getUpper());
-		}
-	}*/
 	
 	public function getLower(){
 		return $this->lowerBound;
@@ -96,8 +61,6 @@ class IntervalObject{
 	}
 
     private function insertChild($index, &$newChild){
-        //echo "INDEX INSERT IS => $index \n";
-        //var_dump($newChild);
         if($index == count($this->children)){
             $this->children[] = $newChild;
         }
@@ -115,7 +78,6 @@ class IntervalObject{
     }
 
 	public function insertInterval(&$name, $lowerBound, $upperBound){
-        //echo "LOWER => $lowerBound HIGHER => $upperBound \n";
 		// if no children
 		if(count($this->children) == 0){
 			$this->children[] = new IntervalObject($lowerBound, $upperBound, $name);
@@ -124,17 +86,13 @@ class IntervalObject{
 		// at least 1 child exists
 		else{
 			$leftNode = $this->getLeftNode($lowerBound);
-			//echo "$leftNode \n";
-			if($leftNode == count($this->children)) $leftNode--;
-			//var_dump($this);
-			//echo 'leftNode => '.$leftNode;
+
+			if($leftNode == count($this->children))
+                $leftNode--;
 			
 			// new is a larger bound OR new is a smaller bound
 			if( ($this->children[$leftNode]->getLower() > $lowerBound && $this->children[$leftNode]->getUpper() > $upperBound) || ($this->children[$leftNode]->getLower() < $lowerBound && $this->children[$leftNode]->getUpper() < $upperBound) ){
-                //array_splice($this->children, $leftNode, 0, -1);
-				//$this->children[$leftNode] = new IntervalObject($lowerBound, $upperBound, $name);
                 $this->insertChild($leftNode+1, new IntervalObject($lowerBound, $upperBound, $name));
-				//$this->sortedInsertChild(new IntervalObject($lowerBound, $upperBound, $name));
 
                 // check if subinterval is large enough to intersect with left
                 if($this->children[$leftNode]->getUpper() - $lowerBound >= CalIntervalDiff::getLength()){
@@ -146,10 +104,6 @@ class IntervalObject{
                     $this->insertIntoChild($leftNode+2, $this->children[$leftNode+2]->getLower(), $upperBound, $name);
                 }
 			}
-			/*else if(){
-				array_splice($this->children, $leftNode+1, 0, -1);
-				$this->children[$leftNode+1] = new IntervalObject($lowerBound, $upperBound, $name);
-			}*/
 			// new is the same interval
 			else if($this->children[$leftNode]->getLower() == $lowerBound && $this->children[$leftNode]->getUpper() == $upperBound){
 				if(is_array($name))
@@ -163,24 +117,12 @@ class IntervalObject{
 			}
 			// new is a subset
 			else if($this->children[$leftNode]->getLower() <= $lowerBound && $this->children[$leftNode]->getUpper() >= $upperBound){
-				//echo 'subset';
 				$this->insertIntoChild($leftNode, $lowerBound, $upperBound, $name);
 			}
 			// new is a superset (or superset multiple)
 			else if($this->children[$leftNode]->getLower() >= $lowerBound && $this->children[$leftNode]->getUpper() <= $upperBound){
-				//echo 'superset';
-                //var_dump($leftNode);
-                //var_dump($lowerBound);
-                //var_dump($upperBound);
-                //var_dump($name);
-				// sorted insert of this properties in new object
-				//array_splice($this->children, $leftNode, 0, -1);
-				//$this->children[$leftNode] = new IntervalObject($this->lowerBound, $this->upperBound, $this->people);
-                //$this->children[$leftNode]->insertInterval($this->children[$leftNode]->people,
-                    //$this->children[$leftNode]->lowerBound, $this->children[$leftNode]->upperBound);
                 $this->insertIntoChild($leftNode, $this->children[$leftNode]->lowerBound,
                     $this->children[$leftNode]->upperBound, $this->children[$leftNode]->people);
-                //$this->sortedInsertChild(new IntervalObject($this->lowerBound, $this->upperBound, $this->people));
 					
 				// replace this properties with the new properties
 				$this->children[$leftNode]->lowerBound = $lowerBound;
@@ -191,35 +133,17 @@ class IntervalObject{
 				else
 					$this->children[$leftNode]->people = array($name);
 
-                //$this->children[$leftNode]->depth++;
-                //if($this->depth <= $this->children[$leftNode]->depth)
-                    //$this->depth = $this->children[$leftNode]->depth+1;
-
 
 				// Check next interval (in case of superset multiple)
 				while(isset($this->children[$leftNode+1]) && $this->children[$leftNode+1]->getLower() >= $lowerBound && $this->children[$leftNode+1]->getUpper() <= $upperBound){
-					//echo "\n\n RIGHT NODE IS -> ".($leftNode+1)."\n\n";
-                    //var_dump($this->children);
-                    //echo "\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
                     $temp = clone $this->children[$leftNode+1];
-					//array_splice($this->children, $leftNode+1, 0, -1);
-					//$this->children[$leftNode+1] = $temp;
-                    //$this->children[$leftNode]->insertInterval($temp->people, $temp->lowerBound, $temp->upperBound);
                     $this->insertIntoChild($leftNode, $temp->lowerBound, $temp->upperBound, $temp->people);
-					//$this->sortedInsertChild(clone $this->children[$leftNode+1]);
 					$this->children = array_merge(array_slice($this->children,0,$leftNode+1), array_slice($this->children,$leftNode+1+1));
                     $leftNode++;
 				}
 
 			}
-			// new is intersection (or intersects multiple)
 			else{
-				echo 'intersect';
-				// intersection means insert normally no matter what
-				//array_splice($this->children, $leftNode, 0, -1);
-				//$this->children[$leftNode] = new IntervalObject($lowerBound, $upperBound, $name);
-                $this->insertChild($leftNode+1, new IntervalObject($lowerBound, $upperBound, $name));
-				
                 die('this should never happen....');
 			}
 			
@@ -241,10 +165,6 @@ class IntervalObject{
 		$beta = count($this->children) - 1;
 		if($beta < 0)
 			$beta = 0;
-		
-		//var_dump($mid);
-		//var_dump($this->children);
-		//echo '+++++++++++++++++++++++++++++++';
 		
 		while($alpha <= $beta){
 			$mid = ((int)(($beta+$alpha)/2));
