948d24adb7cedd9a173eb66ef9134b2cfd921dcc
Samuel Hunter <swilkinsonhunter@gmail.com>
2015-12-11 20:51:40 -0600

    can now add contacts to groups via drop down

--- frontend/app/scripts/controllers/groups.js
+++ frontend/app/scripts/controllers/groups.js
@@ -15,6 +15,7 @@ angular.module('easyncApp')
   	$scope.selectedgroup = [];
   	$scope.groupname = '';
   	$scope.newRow = false;
+  	$scope.contacts = [];
     
     //load groups from DB and display them
     $scope.loadGroups = function() {
@@ -44,33 +45,37 @@ angular.module('easyncApp')
     	$scope.newRow = true;
     };
 
-    $scope.saverow = function(newName, newEmail) {
+    $scope.saverow = function(newName, newEmail, newContact) {
     	//save new contact to contact list
-    	var data = {'name': newName, 'email': newEmail, 'groupName': $scope.groupname};
-    	var json_data = JSON.stringify(data);
+    	newContact = JSON.parse(newContact);
 
-    	var handleSuccessFindingUser = function(response) {
-    		if (response.data) {
-	    		$http({
-	    			method: 'POST',
-	    			url: GlobalIPService.ip + 'api/v1.0/Group/addContactToGroup',
-	    			data: json_data,
-	    			withCredentials: true
-	    		}).then(function (response) {
-	    			if (response.data === 'Person Added to Group') {
-	    				$scope.newRow = false;
-	    				$scope.selectedgroup.push({'name': newName, 'email': newEmail});
-	    				$scope.groups[$scope.groupname] = $scope.selectedgroup;
-	    			}
-	    		}, function (error) {
-	    			console.log(error);
-	    		});
-	    	} else {
-	    		console.log("user does not exist", response.data);
-	    	}
+    	var handleSuccessFindingUser = function(json_data) {
+    		$http({
+    			method: 'POST',
+    			url: GlobalIPService.ip + 'api/v1.0/Group/addContactToGroup',
+    			data: json_data,
+    			withCredentials: true
+    		}).then(function (response) {
+    			if (response.data === 'Person Added to Group') {
+    				$scope.newRow = false;
+    				if (newContact) {
+    					$scope.selectedgroup.push({'name': newContact.name, 'email': newContact.email});
+    				} else {
+    					$scope.selectedgroup.push({'name': newName, 'email': newEmail});
+    				}
+    				
+    				$scope.groups[$scope.groupname] = $scope.selectedgroup;
+    			}
+    		}, function (error) {
+    			console.log(error);
+    		});
     	};
 
-    	if (newName !== '' && newEmail !== '') {
+    	if (newContact !== undefined) {
+    		var data = {'name': newContact["name"], 'email': newContact["email"], 'groupName': $scope.groupname};
+    		var json_data = JSON.stringify(data);
+    		handleSuccessFindingUser(json_data);
+    	} else if (newName !== '' && newEmail !== '') {
     		var exists_data = {email : newEmail};
 
     		$http({
@@ -78,13 +83,33 @@ angular.module('easyncApp')
     			url: GlobalIPService.ip + 'api/v1.0/User/exists',
     			params: exists_data,
     			withCredentials: true
-    		}).then(handleSuccessFindingUser, function (error) {
+    		}).then(function (response) {
+    			if (response.data) {
+    				var data = {'name': newName, 'email': newEmail, 'groupName': $scope.groupname};
+    				var json_data = JSON.stringify(data);
+    				handleSuccessFindingUser(json_data);
+    			} else {
+    				console.log("user does not exist", response.data);
+    			}
+    		}, function (error) {
     			console.log(error);
     		});
     	}
-
-
-    	
     };
 
+    $scope.loadcontacts = function () {
+    	$http({
+			url: GlobalIPService.ip + 'api/v1.0/User/getContactsInfo',
+			method: 'GET',
+			withCredentials: true
+		}).then(function (response) {
+			for (var i = 0; i < response.data.length; i++) {
+                if (response.data[i].name !== undefined && response.data[i].email !== undefined) {
+                    $scope.contacts.push({'name': response.data[i].name, 'email' : response.data[i].email});
+                }
+            }
+		}, function (error) {
+			console.log(error);
+		});
+    };
 });
