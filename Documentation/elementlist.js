2d9b00043cddf7ca45d14386ea109fdc91a390d7
cahenk <cahenk95@gmail.com>
2015-12-13 23:28:39 -0600

    htaccess updated to allow documentation URL's through

---
+++ Documentation/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Binding"],["c","CalIntervalDiff"],["c","CRUD"],["c","Database"],["c","Event"],["c","GoogleCalendar"],["c","Group"],["c","IntervalObject"],["c","Model"],["c","Permission"],["f","rglob()"],["c","User"]];
